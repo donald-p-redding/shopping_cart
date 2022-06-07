5973d589e1b8d2e9a8b920a556ae7e2ae363de72
Pauline3414159 <fagie37@hotmail.com>
2022-06-07 11:14:24 -0700

    edit form is controlled

--- client/src/components/EditProductForm.js
+++ client/src/components/EditProductForm.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 const EditProductForm = ({ info, visible, toggle }) => {
-  const { title, quantity, price } = info
+  const { title, quantity, price, _id } = info
+
+  const [productInfo, setProductInfo] = useState({title, quantity, price})
+
+  const titleChange = (e) => {
+    setProductInfo(
+      {...productInfo,
+      title: e.target.value}
+    )
+  }
+
+  const quantityChange = (e) => {
+    setProductInfo(
+      {...productInfo,
+      quantity: e.target.value}
+    )
+  }
+
+  const priceChange = (e) => {
+    setProductInfo(
+      {...productInfo,
+      price: e.target.value}
+    )
+  }
 
   const classType = visible ? 'edit-form visible' : 'edit-form';
 
@@ -11,22 +34,32 @@ const EditProductForm = ({ info, visible, toggle }) => {
       <form>
         <div class="input-group">
           <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value={title} />
+          <input type="text" id="product-name" value={productInfo.title} onChange={titleChange}/>
         </div>
 
         <div class="input-group">
           <label for="product-price">Price</label>
-          <input type="text" id="product-price" value={price} />
+          <input type="text" id="product-price" value={productInfo.price} onChange={priceChange}/>
         </div>
 
         <div class="input-group">
           <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value={quantity} />
+          <input type="text" id="product-quantity" value={productInfo.quantity} onChange={quantityChange}/>
         </div>
 
         <div class="actions form-actions">
-          <button className="button">Update</button>
-          <button onClick={toggle} className="button">Cancel</button>
+          <button 
+          className="button"
+          onClick={(e) => {
+            e.preventDefault()
+            console.log('updating info')
+          }}>
+            Update
+            </button>
+          <button onClick={(e)=> {
+            e.preventDefault()
+            toggle()
+          }} className="button">Cancel</button>
         </div>
       </form>
     </div>
